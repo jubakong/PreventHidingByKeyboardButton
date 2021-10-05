fee6cfc5cd8b1dac82ea5b50699571f72704c4bf
jubakong <jubakong@gmail.com>
2021-10-05 10:39:01 +0900

    Add 'PreventHidingByKeyboardButton.swift'

---
+++ PreventHidingByKeyboardButton/PreventHidingByKeyboardButton.swift
@@ -0,0 +1,12 @@
+//
+//  PreventHidingByKeyboardButton.swift
+//  PreventHidingByKeyboardButton
+//
+//  Created by Jeongbae Kong on 2021/10/05.
+//
+
+import UIKit
+
+class PreventHidingByKeyboardButton: UIButton {
+  
+}
